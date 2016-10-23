c80a8a8beb252976eda430ed7a57f27e6840b790
rubyist <rubyist.personal@openmailbox.org>
2016-10-23 16:44:29 -0700

    Added preamble for ARM Linux

---
+++ asm/arm-linux-gnueabihf-preamble.s
@@ -0,0 +1,23 @@
+	.section	.text
+	.globl	_start
+	.globl	bfmain
+_start:
+	mov	r5, #0
+	bl	bfmain
+	mov	r0, #0
+	mov	r7, #1
+	swi	#0
+bfgetchar:
+	mov	r0, #0
+	mov	r1, r4
+	mov	r2, #1
+	mov	r7, #3
+	swi	#0
+	b	lr
+bfputchar:
+	mov	r0, #1
+	mov	r1, r4
+	mov	r2, #1
+	mov	r7, #4
+	swi	#0
+	b	lr
